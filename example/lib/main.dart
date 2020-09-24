703ba9960ac826b35a3be9b70680ad411f9ad888
Rakesh Chevuru <Rakesh.c@ctstracking.com>
2020-09-24 11:16:38 -0600

    added app retain for android

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ble_lib_example/devices_list/devices_bloc_provider.dart';
 import 'package:flutter_ble_lib_example/devices_list/devices_list_view.dart';
+import 'package:flutter_ble_lib_example/util/app_retain_widget.dart';
 
 import 'device_details/device_detail_view.dart';
 import 'device_details/devices_details_bloc_provider.dart';
@@ -22,13 +23,7 @@ class MyApp extends StatelessWidget {
         primaryColor: new Color(0xFF0A3D91),
         accentColor: new Color(0xFFCC0000),
       ),
-      initialRoute: "/",
-      routes: <String, WidgetBuilder>{
-        "/": (context) => DevicesBlocProvider(child: DevicesListScreen()),
-        "/details": (context) =>
-            DeviceDetailsBlocProvider(child: DeviceDetailsView()),
-      },
-      navigatorObservers: [routeObserver],
+      home: AppRetainWidget(child: DevicesBlocProvider(child: DevicesListScreen())),
     );
   }
 }
